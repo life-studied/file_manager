df16e507af3cdfb269a4c82ef11c5b2a82eca753
Jay_d <10457987+jay-d@user.noreply.gitee.com>
2023-04-18 18:30:56 +0800

    解决返回值消失问题

--- FileManager/file_manager.cpp
+++ FileManager/file_manager.cpp
@@ -1,15 +1,14 @@
 #include "file_manager.h"
-#include <fstream>
 
 file_manager::file_manager() {}
 
-string&& file_manager::getData(string filename) throw(std::ifstream::failure)
+string file_manager::getData(string filename) throw(std::ifstream::failure)
 {
 	std::ifstream streamReader(filename, std::ios::binary);
 	if (!streamReader.is_open())
 		throw std::ifstream::failure("file couldn't open");
 	streamReader.seekg(0, std::ios::end);		//游标移到文件结尾
-	unsigned filesize = streamReader.tellg();			//获取游标当前位置 - 文件开始位置，此处为文件大小
+	std::streamoff filesize = streamReader.tellg();		//获取游标当前位置 - 文件开始位置，此处为文件大小
 	char* _data = new char[filesize];					//分配内存
 	streamReader.seekg(0, std::ios::beg);		//跳转回开始
 	streamReader.read(_data, filesize);		//读取文件
@@ -19,7 +18,7 @@ string&& file_manager::getData(string filename) throw(std::ifstream::failure)
 	return std::move(data);
 }
 
-std::string& file_manager::replaceAll(std::string& context, const std::string& from, const std::string& to)
+string& file_manager::replaceAll(string& context, const string& from, const string& to)
 {
 	size_t lookHere = 0;
 	size_t foundHere;
--- FileManager/file_manager.h
+++ FileManager/file_manager.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <string>
+#include <fstream>
 using std::string;
-using std::wstring;
 class file_manager
 {
 public:
 	file_manager();
-	string&& getData(string filename);
-	std::string& replaceAll(
-		std::string& context,
-		const std::string& from,
-		const std::string& to);
+	string getData(string filename) throw(std::ifstream::failure);
+	string& replaceAll(
+		string& context,
+		const string& from,
+		const string& to);
 };
 
